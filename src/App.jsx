7e35b7a18fbd924c25e6b5f1f36148e2b11b5134
abdinoor12345 <mohamedabdinoor873@gmail.com>
2026-05-25 03:33:20 -0700

    feat: setup report metrics dashboard with local chart assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
-import Navbar from "./Navbar"
+ import Navbar from "./Navbar"
 import LandingPage from "./pages/LandingPage"
 import { StrictMode } from "react"
 import { Routes, Route } from "react-router-dom"
 import Article1 from "./pages/Article1"
-import Article2 from "./pages/Article2" // This is where your Report view code goes!
+import Article2 from "./pages/Article2"
 import Article3 from "./pages/Article3"
 import Article4 from "./pages/Article4"
 import Article5 from "./pages/Article5"
 import Report from "./pages/report"
-
-function App() {
+ function App() {
   return (
     <Routes>
-      {/* Added /dataforrights prefix to your routes */}
-      <Route path="/dataforrights" element={<LandingPage />} />
-      <Route path="/dataforrights/article1" element={<Article1 />} />
-      <Route path="/dataforrights/article2" element={<Article2 />} />
-      <Route path="/dataforrights/article3" element={<Article3 />} />
-      <Route path="/dataforrights/article4" element={<Article4 />} />
-      <Route path="/dataforrights/article5" element={<Article5 />} />
-      <Route path="/dataforrights/report" element={<Report />} />
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/article1" element={<Article1 />} />
+      <Route path="/article2" element={<Article2 />} />
+      <Route path="/article3" element={<Article3 />} />
+      <Route path="/article4" element={<Article4 />} />
+      <Route path="/article5" element={<Article5 />} />
+      <Route path="/report" element={<Report />} />
     </Routes>
   )
 }
